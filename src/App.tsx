6c8e815077d3511ade3ac8058aacb645a39a001e
Brandon Lim Jing Zhi <brandonlimjingzhi@gmail.com>
2023-03-30 18:42:47 +0800

    Code for Text Bubbles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
+  const [displayedMessages, setDisplayedMessages] = React.useState<string[]>([]);
+  const [inputMessage, setInputMessage] = React.useState<string>("");
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setDisplayedMessages(displayedMessages.slice(1));
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [displayedMessages]);
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+
+      if (inputMessage.length > 0) {
+        setDisplayedMessages([...displayedMessages, inputMessage]);
+        setInputMessage("");
+        document.getElementById("input-message")!.innerHTML = "";
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="bg-[#00FF00] text-3xl flex flex-col justify-end min-h-screen gap-5 p-5 font-inter">
+      <div className="flex flex-col gap-5">
+        {displayedMessages.map((message, i) => {
+          return (
+            <p className="max-w-md p-3 break-words whitespace-pre-wrap bg-white rounded-xl w-fit" key={i}>
+              {message}
+            </p>
+          );
+        })}
+      </div>
+      <div
+        className={`${
+          inputMessage
+            ? "rounded-xl w-fit rounded-bl-none text-black break-words max-w-md whitespace-pre-wrap bg-white opacity-100"
+            : "text-[#00FF00] opacity-0"
+        }
+            outline-none p-3 transition-opacity duration-300 ease-in-out`}
+        id="input-message"
+        contentEditable="true"
+        spellCheck="false"
+        onInput={(event) => setInputMessage(event.currentTarget.textContent!)}
+        onKeyDown={(event) => handleKeyDown(event)}
+      />
+    </main>
   );
 }
-
-export default App;
